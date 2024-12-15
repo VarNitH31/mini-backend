5a03f6d63cdeeb3245b54baab30e16587f332fdf
VarNitH31 <varnithdramesh@gmail.com>
2024-12-15 22:25:24 +0530

    ready to deploy

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\varni\Desktop\5th sem\mini project\Mini_project\job-title-prediction\venv
+set VIRTUAL_ENV=C:\Users\varni\Desktop\5th sem\mini project\mini-backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
